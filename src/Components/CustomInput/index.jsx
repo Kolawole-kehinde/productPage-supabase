411a14abbd117839c0d4d4a2ccf2955ee7be7b15
kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2025-03-26 11:21:52 +0100

    Implemented password visibility toggle functionality in the CustomInput component to enhance user experience on password input fields.

--- src/Components/CustomInput/index.jsx
+++ src/Components/CustomInput/index.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaEyeSlash } from 'react-icons/fa';
+import { IoEyeSharp } from 'react-icons/io5';
 
 const CustomInput  = ({
     label,
@@ -8,16 +10,31 @@ const CustomInput  = ({
     register  = () => {},
     error
 }) => {
+    const [showPassword, setShowPassword] = useState(false);
+  const togglePassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
   return (
     <div className='space-y-4'>
        {label && <label htmlFor={name}>{label}</label>}
-       <div>
+       <div className='relative'>
        <input
-        type={type}
+        type={showPassword && type === "password" ? "text" : type}
         name={name}
         className={`mt-1 w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
         {...register(name)}
       />
+      {
+        type === "password" && (
+            <div className='absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer' onClick={togglePassword}>
+                {showPassword ? (
+                    <IoEyeSharp />
+                ) : (
+                    <FaEyeSlash />
+                )}
+            </div>
+        )
+      }
        </div>
       {error && <p className="text-red-500 text-sm">{error.message}</p>}
 
